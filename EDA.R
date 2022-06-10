07615d099fa538c28783efaee6447a9febcad607
Siv Hansen <sivcartland@hotmail.dk>
2022-06-10 10:54:18 +0200

    data cleaning

--- EDA.R
+++ EDA.R
@@ -3,7 +3,7 @@ library("ggplot2")
 # Read data 
 ames <- read.csv("ameshouse.txt", sep="")
 ames$SalePrice <- ames$SalePrice/1000
-df <- data.frame(ames)
+ames <- data.frame(ames)
 
 # Stats
 print(ncol(ames))
@@ -14,19 +14,24 @@ str(ames)
 require(gridExtra)
 options(scipen=10000)
 p1 <- ggplot(df, aes(x=SalePrice)) + geom_density() + labs(x="Sale Price (in thousand $)")
-p2 <- ggplot(df, aes(x=SalePrice)) + geom_density() +  scale_x_log10() + labs(x="log(Sale Price)", y = "")
+p2 <- ggplot(df, aes(x=SalePrice)) + geom_density() +  scale_x_log10()  + labs(x="log(Sale Price)", y = "")
 grid.arrange(p1, p2, ncol=2)
 
 # Check number of missing values 
 data.frame(sort(colSums(is.na(ames)), decreasing = TRUE)[0:20])
 
-# remove rows with missing values 
+# Remove features FireplaceQu, LotFrontage, secondary garage features
+drop_cols <- c("FireplaceQu","LotFrontage", "GarageType", "GarageYrBlt","GarageFinish","GarageQual","GarageCond")
+ames <- ames[,!(names(ames) %in% drop_cols)]
+
+# Remove rows with missing values 
 ames <- ames[which(complete.cases(ames)),]
+
+# Check for missing values
+data.frame(sort(colSums(is.na(ames)), decreasing = TRUE)[0:20])
 
 # Scatter plot of continous variables
 nums <- unlist(lapply(ames, is.numeric)) 
 ames_num <- ames[ , nums]
-
 plot(ames$SalePrice, ames$nums[2], ylab = ames_num[2])
-
 pairs(ames_num)
